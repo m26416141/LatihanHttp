a0582817d6bc8cc0c4d72a0b96b61b78150e987d
m26416141 <m26416141@john.petra.ac.id>
2021-04-27 20:46:09 +0800

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,37 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public dataPOST = [];
+  public loading : any;
+
+  constructor(private http : HttpClient, private loadCtrl : LoadingController) {
+  }
+
+  public async loaderPresent(): Promise<any> {
+    const loading = await this.loadCtrl.create({
+      message: "loading...",
+      backdropDismiss: true
+    });
+    await loading.present();
+
+    return loading;
+  }
+
+  async getDataPost() {
+    this.loading = await this.loaderPresent();
+
+    this.http.get("https://reqres.in/api/users").subscribe((res : any) => {
+      console.log(res);
+      this.dataPOST = res.data;
+
+      if (this.loading) {
+        this.loading.dismiss();
+      }
+    });
+  }
+
+  ionViewDidEnter() {
+    this.getDataPost();
+  }
 
 }
