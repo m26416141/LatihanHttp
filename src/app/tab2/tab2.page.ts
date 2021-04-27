08410dbccc961fdf6698bb9de9b848211a740201
m26416141 <m26416141@john.petra.ac.id>
2021-04-27 21:07:36 +0800

    add *unfinished

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { LoadingController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,30 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  public dataPOST = [];
+  public loading : any;
+  post : any = {};
+
+  constructor(private http : HttpClient, private loadCtrl : LoadingController, private toastCtrl : ToastController) {}
+
+  public async loaderPresent(): Promise<any> {
+    const loading = await this.loadCtrl.create({
+      message: "loading...",
+      backdropDismiss: true
+    });
+    await loading.present();
+
+    return loading;
+  }
+
+  submit() {
+    this.http.post("https://reqres.in/api/users", this.post).subscribe((res:any) => {
+      console.log(res);
+      this.toastCtrl.create({
+        duration : 3000,
+        message : "New item ID: "+ res.id
+      }).then(l => l.present())
+    });
+  }
 
 }
